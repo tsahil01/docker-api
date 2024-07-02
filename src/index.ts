5384ca34b7e42904f072771f23f8ee2e61a32acb
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2024-07-02 19:40:42 +0530

    create, list all, delete containers

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,37 @@
-console.log("Hello");
+import Docker from 'dockerode';
+
+const docker = new Docker();
+
+async function startOne(){
+
+    // Create a new container
+    const newContainer = await docker.createContainer({
+        Image: 'hello-world',
+        name: 'hello-world'
+    });
+    console.log(newContainer);
 
 
+    // list all containers
+    const container = await docker.listContainers({
+        all: true
+    });
+    console.log(container)
 
 
+    // delete all containers 
+    container.forEach(async (container) => {
+        await docker.getContainer(container.Id).remove({
+            force: true
+        }, (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(data);
+            }
+        });
+    });
+
+}
+
+startOne();
